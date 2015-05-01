0ef982864d85890acd061cc4fe31eb9b5320ade5
Dan Prince <dprince@redhat.com>
2015-05-01 12:22:31 -0400

    Revert "Keystone domain for Heat"
    This reverts commit 7313930c22b9f18d67e630de084ffcc6fad5ebe7.
    
    Seeing errors when trying to create the keystone admin
    role with packages. (ImportError: No module named os_client_config)
    
    Change-Id: I78796598ccb8d2ffd6bfca85dce7d18dc0fd768e
    Related-bug: #1450786

--- puppet/manifests/overcloud_controller.pp
+++ puppet/manifests/overcloud_controller.pp
@@ -260,7 +260,6 @@ if hiera('step') >= 2 {
 if hiera('step') >= 3 {
 
   include ::keystone
-  include ::keystone::roles::admin
 
   #TODO: need a cleanup-keystone-tokens.sh solution here
   keystone_config {
@@ -462,9 +461,6 @@ if hiera('step') >= 3 {
   include ::heat::api_cfn
   include ::heat::api_cloudwatch
   include ::heat::engine
-  # TO-DO: Remove this class as soon as Keystone v3 will be fully functional
-  include ::heat::keystone::domain
-  Service['keystone'] -> Class['::keystone::roles::admin'] -> Exec['heat_domain_create']
 
   $snmpd_user = hiera('snmpd_readonly_user_name')
   snmp::snmpv3_user { $snmpd_user:
