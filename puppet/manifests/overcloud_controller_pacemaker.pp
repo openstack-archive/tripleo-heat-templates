97feaf9fe9f7a8ed2ae3b12c51a660366d94bba1
Dan Prince <dprince@redhat.com>
2016-01-18 08:55:39 -0500

    puppet: allow config of ad-hoc Ceph settings
    Including ::ceph::conf on ceph roles will allow ad-hoc
    (non-puppet managed) settings to be made in the
    ceph.conf using Hiera.
    
    Change-Id: I656a0ecde465023d7afad9371aa3c5c270078a67
    (cherry picked from commit 867b36625e38972e7a74989134a1f89dcbdc0fe7)

--- puppet/manifests/overcloud_controller_pacemaker.pp
+++ puppet/manifests/overcloud_controller_pacemaker.pp
@@ -464,6 +464,7 @@ MYSQL_HOST=localhost\n",
     class { '::ceph::profile::params':
       mon_initial_members => downcase(hiera('ceph_mon_initial_members')),
     }
+    include ::ceph::conf
     include ::ceph::profile::mon
   }
 
@@ -482,10 +483,12 @@ MYSQL_HOST=localhost\n",
       } -> Class['ceph::profile::osd']
     }
 
+    include ::ceph::conf
     include ::ceph::profile::osd
   }
 
   if str2bool(hiera('enable_external_ceph', false)) {
+    include ::ceph::conf
     include ::ceph::profile::client
   }
 
