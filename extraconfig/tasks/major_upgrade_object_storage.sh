c78a215ae9cd02a47e46b4d302f2a275ccce1693
marios <marios@redhat.com>
2016-03-02 18:09:59 +0200

    Introduce a UpgradeScriptDeliveryWorfklow as part of tripleo upgrades
    This splits the upgrade script delivery out of the UpgradeWorkflow
    and into a new task which delivers the upgrade script for
    compute and object-storage nodes. This is intended to be the first
    part of the upgrades process, since we need to upgrade swift nodes
    before the controllers and then only one at a time. So this will
    deliver the upgrade script which can be invoked by the operator
    using the existing script in tripleo-common
    'upgrade-non-controller.sh'.
    
    This can be invoked by passing the -e
    environments/major-upgrade-script-delivery.yaml (added here) to
    the openstack overcloud deploy command.
    
    Change-Id: I20a0d4978e907111404f8108c502ab53b69a3296

---
+++ extraconfig/tasks/major_upgrade_object_storage.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+#
+# This delivers the swift-storage upgrade script to be invoked as part of the tripleo
+# major upgrade workflow.
+#
+set -eu
+
+UPGRADE_SCRIPT=/root/tripleo_upgrade_node.sh
+
+cat > $UPGRADE_SCRIPT << ENDOFCAT
+### DO NOT MODIFY THIS FILE
+### This file is automatically delivered to the swift-storage nodes as part of the
+### tripleo upgrades workflow
+
+
+function systemctl_swift {
+    action=$1
+    for S in openstack-swift-account-auditor openstack-swift-account-reaper openstack-swift-account-replicator openstack-swift-account \
+             openstack-swift-container-auditor openstack-swift-container-replicator openstack-swift-container-updater openstack-swift-container \
+             openstack-swift-object-auditor openstack-swift-object-replicator openstack-swift-object-updater openstack-swift-object openstack-swift-proxy; do
+                systemctl $action $S
+    done
+}
+
+
+systemctl_swift stop
+
+yum -y update
+
+systemctl_swift start
+
+
+
+ENDOFCAT
+
+# ensure the permissions are OK
+chmod 0755 $UPGRADE_SCRIPT
+
