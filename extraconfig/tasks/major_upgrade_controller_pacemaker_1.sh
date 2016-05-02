296782c1d60908650be45fa16c4d6cac1048ec90
Ian Pilcher <arequipeno@gmail.com>
2016-05-02 16:21:55 -0500

    Disable VIPs before stopping cluster during version upgrade
    If "pcs cluster stop --all" is executed on a controller that
    happens to have a VIP on the internal network, pcs may use the
    VIP as the source address for communication with another cluster
    node.  When pacemaker is stopped this VIP goes away, and pcs never
    receives a response from the other node.  This causes pcs to hang
    indefinitely; eventually the upgrade times out and fails.
    
    Disabling the VIPs before stopping the cluster avoids this
    situation.
    
    Change-Id: I6bc59120211af28456018640033ce3763c373bbb
    Closes-Bug: 1577570
    (cherry picked from commit 6e65c8fc0a3c2b4025b08d1a2c8b03696e8fb7f6)

--- extraconfig/tasks/major_upgrade_controller_pacemaker_1.sh
+++ extraconfig/tasks/major_upgrade_controller_pacemaker_1.sh
@@ -26,6 +26,12 @@ if [ "$(hiera -c /etc/puppet/hiera.yaml bootstrap_nodeid)" = "$(facter hostname)
     check_resource memcached stopped 600
     pcs resource disable galera
     check_resource galera stopped 600
+    # Disable all VIPs before stopping the cluster, so that pcs doesn't use one as a source address:
+    #   https://bugzilla.redhat.com/show_bug.cgi?id=1330688
+    for vip in $(pcs resource show | grep ocf::heartbeat:IPaddr2 | grep Started | awk '{ print $1 }'); do
+      pcs resource disable $vip
+      check_resource $vip stopped 60
+    done
     pcs cluster stop --all
 fi
 
