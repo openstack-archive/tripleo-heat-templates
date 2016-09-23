6d17088192d859c22880380ad3fa3a668eec9292
marios <marios@redhat.com>
2016-09-23 17:19:07 +0300

    Adds Environment File for Removing Sahara during M/N upgrade
    The default path if the operator does nothing is to keep the
    sahara services on mitaka to newton upgrades.
    
    If the operator wishes to remove sahara services then they
    need to specify the provided major-upgrade-remove-sahara.yaml
    environment file in the stack upgrade commands.
    
    The existing migration to ha arch already removes the constraints
    and pcs resource for sahara api/engine so we just need to stop
    it from starting again if we want to remove it.
    
    This adds a  KeepSaharaServiceOnUpgrade parameter to determine if
    Sahara is disabled from starting up after the controllers are
    upgraded (defaults true).
    
    Finally it is worth noting that we default the sahara services
    as 'on' during converge here in the resource_registry of the
    converge environment file; any subsequent stack updates where
    the deployment contains sahara services will need to
    include the -e /environments/services/sahara.yaml environment
    file.
    
    Related-Bug: 1630247
    Change-Id: I59536cae3260e3df52589289b4f63e9ea0129407
    (cherry picked from commit 2e6cc07c1a74c2dd7be70568f49834bace499937)

--- extraconfig/tasks/major_upgrade_controller_pacemaker_3.sh
+++ extraconfig/tasks/major_upgrade_controller_pacemaker_3.sh
@@ -16,7 +16,11 @@ systemctl_swift start
 # We need to start the systemd services we explicitely stopped at step _1.sh
 # FIXME: Should we let puppet during the convergence step do the service enabling or
 # should we add it here?
-for service in $(services_to_migrate); do
+services=$(services_to_migrate)
+if [[ ${keep_sahara_services_on_upgrade} =~ [Ff]alse ]] ; then
+    services=${services%%openstack-sahara*}
+fi
+for service in $(services); do
     manage_systemd_service start "${service%%-clone}"
     check_resource_systemd "${service%%-clone}" started 600
 done
