1a425749bc36d8b808ccd54c5aef815f1b4c0e93
Pradeep Kilambi <pkilambi@redhat.com>
2016-10-19 07:32:25 -0400

    Rework gnocchi-upgrade to run in a separate upgrade step
    gnocchi when configured with swift will require keystone
    to be available to authenticate to migrate to v3. At this
    step keystone is not available and gnocchi upgrade fails
    with auth error. Instead start apache in step 3, start
    apache first and then run gnocchi upgrade in a separate
    step and let upgrade happen here.
    
    Closes-Bug: #1634897
    
    Change-Id: I22d02528420e4456f84b80905a7b3a80653fa7b0
    (cherry picked from commit a8e119094f5477c132220e30e7a2e1a9915581a3)

--- extraconfig/tasks/major_upgrade_controller_pacemaker_3.sh
+++ extraconfig/tasks/major_upgrade_controller_pacemaker_3.sh
@@ -9,18 +9,9 @@ check_resource redis started 600
 start_or_enable_service openstack-cinder-volume
 check_resource openstack-cinder-volume started 600
 
+# start httpd so keystone is available for gnocchi
+# upgrade to run.
+systemctl start httpd
 
 # Swift isn't controled by pacemaker
 systemctl_swift start
-
-# We need to start the systemd services we explicitely stopped at step _1.sh
-# FIXME: Should we let puppet during the convergence step do the service enabling or
-# should we add it here?
-services=$(services_to_migrate)
-if [[ ${keep_sahara_services_on_upgrade} =~ [Ff]alse ]] ; then
-    services=${services%%openstack-sahara*}
-fi
-for service in $services; do
-    manage_systemd_service start "${service%%-clone}"
-    check_resource_systemd "${service%%-clone}" started 600
-done
