61cba946cd11ab3a2d6518663f2300d7c98083de
Mathieu Bultel <mbultel@redhat.com>
2016-10-26 11:21:08 +0200

    Add replacepkgs to the manual ovs upgrade workaround and fix a typo
    rpm command will return an exit 1 if ovs package is already
    there and will exit the step_1.sh script. To get around this
    force the update with --replacepkgs
    
    Also remove the \ just before the $ which cause a syntax
    error for the ceph storage
    
    Change-Id: I11fcf688982ceda5eef7afc8904afae44300c2d9
    Closes-bug: 1636748

--- extraconfig/tasks/major_upgrade_ceph_storage.sh
+++ extraconfig/tasks/major_upgrade_ceph_storage.sh
@@ -50,14 +50,14 @@ timeout 60 bash -c "while kill -0 ${OSD_PIDS} 2> /dev/null; do
 done"
 
 # Special-case OVS for https://bugs.launchpad.net/tripleo/+bug/1635205
-if [[ -n \$(rpm -q --scripts openvswitch | awk '/postuninstall/,/*/' | grep "systemctl.*try-restart") ]]; then
+if [[ -n $(rpm -q --scripts openvswitch | awk '/postuninstall/,/*/' | grep "systemctl.*try-restart") ]]; then
     echo "Manual upgrade of openvswitch - restart in postun detected"
     mkdir OVS_UPGRADE || true
     pushd OVS_UPGRADE
     echo "Attempting to downloading latest openvswitch with yumdownloader"
     yumdownloader --resolve openvswitch
-    echo "Updating openvswitch with nopostun"
-    rpm -U --nopostun ./*.rpm
+    echo "Updating openvswitch with nopostun option"
+    rpm -U --replacepkgs --nopostun ./*.rpm
     popd
 else
     echo "Skipping manual upgrade of openvswitch - no restart in postun detected"
