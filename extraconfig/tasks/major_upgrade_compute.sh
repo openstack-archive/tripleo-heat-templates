afcb6e01f3af573a7bdd286a65b71eee48cec204
marios <marios@redhat.com>
2016-11-18 19:18:54 +0200

    Make the openvswitch 2.4->2.5 upgrade more robust
    In I9b1f0eaa0d36a28e20b507bec6a4e9b3af1781ae and
    I11fcf688982ceda5eef7afc8904afae44300c2d9 we added a manual step
    for upgrading openvswitch in order to specify the --nopostun
    as discussed in the bug below.
    
    This change adds a minor update to make this workaround more
    robust. It removes any existing rpms that may be around from
    an earlier run, and also checks that the rpms installed are
    at least newer than the version we are on.
    
    This also refactors the code into a common definition in the
    pacemaker_common_functions.sh which is included even for the
    heredocs generating upgrade scripts during init. Thanks
    Sofer Athlan-Guyot and Jirka Stransky for help with that.
    
    Change-Id: Idc863de7b5a8c116c990ee8c1472cfe377836d37
    Related-Bug: 1635205

--- extraconfig/tasks/major_upgrade_compute.sh
+++ extraconfig/tasks/major_upgrade_compute.sh
@@ -18,19 +18,8 @@ set -eu
 
 crudini  --set /etc/nova/nova.conf upgrade_levels compute $upgrade_level_nova_compute
 
-# Special-case OVS for https://bugs.launchpad.net/tripleo/+bug/1635205
-if [[ -n \$(rpm -q --scripts openvswitch | awk '/postuninstall/,/*/' | grep "systemctl.*try-restart") ]]; then
-    echo "Manual upgrade of openvswitch - restart in postun detected"
-    mkdir OVS_UPGRADE || true
-    pushd OVS_UPGRADE
-    echo "Attempting to downloading latest openvswitch with yumdownloader"
-    yumdownloader --resolve openvswitch
-    echo "Updating openvswitch with nopostun option"
-    rpm -U --replacepkgs --nopostun ./*.rpm
-    popd
-else
-    echo "Skipping manual upgrade of openvswitch - no restart in postun detected"
-fi
+$(declare -f special_case_ovs_upgrade_if_needed)
+special_case_ovs_upgrade_if_needed
 
 yum -y install python-zaqarclient  # needed for os-collect-config
 yum -y update
