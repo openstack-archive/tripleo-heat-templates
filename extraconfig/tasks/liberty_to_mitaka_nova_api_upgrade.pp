87e6e5ae787732556c4737016b4f13fc93e777b5
Mathieu Bultel <mbultel@redhat.com>
2016-12-08 16:10:32 +0100

    Create nova_api db during the controller upgrade
    For liberty to mitaka upgrade, we need to create the
    nova api database during the controller upgrade to avoid
    interruption of the compute services before the converge.
    Once the compute nodes are updated, any action on the nova
    instance is impossible because the api_db isn't create it
    (created during the converge step)
    
    Change-Id: I7ef4d16173d34f00c8d10d0e414da960094cb4ae
    Closes-Bug: #1648504

---
+++ extraconfig/tasks/liberty_to_mitaka_nova_api_upgrade.pp
@@ -0,0 +1,37 @@
+# Copyright 2015 Red Hat, Inc.
+# All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+# This puppet manifest is to be used only during a Liberty->Mitaka upgrade
+# It creates the nova api database during the controller upgrade instead of
+# during the converge step.
+
+class { '::mysql::server':
+  service_manage => false,
+}
+
+include ::tripleo::packages
+$pacemaker_master   = hiera('bootstrap_nodeid')
+
+if downcase($pacemaker_master) == $::hostname {
+    class {'::nova::db::mysql_api':
+        user          => hiera('nova::db::mysql_api::dbname'),
+        password      => hiera('nova::db::mysql_api::password'),
+        dbname        => hiera('nova::db::mysql_api::dbname'),
+        host          => hiera('nova_api_vip'),
+        charset       => 'utf8',
+        collate       => 'utf8_general_ci',
+        allowed_hosts => undef,
+      }
+}
