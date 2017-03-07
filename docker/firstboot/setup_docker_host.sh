46b5911a150aa0ce89629c3058e3ad65b2d98684
Martin André <m.andre@redhat.com>
2017-03-07 16:06:30 +0100

    Enable Docker service for Compute role
    A recent commit [1] change how docker is installed and configured on
    the overcloud nodes, from a cloud-init script to a proper puppet
    profile in puppet-tripleo but forgot to enable the docker service on
    the compute nodes.
    
    [1] Ia50169819cb959025866348b11337728f8ed5c9e
    
    Change-Id: I202723d0e48f110e5b0dbfe3dcf6646da9f37948

--- docker/firstboot/setup_docker_host.sh
+++ docker/firstboot/setup_docker_host.sh
@@ -1,26 +1,8 @@
 #!/bin/bash
 set -eux
-# TODO This would be better in puppet
+# This file contains setup steps that can't be or have not yet been moved to
+# puppet
 
-# TODO remove this when built image includes docker
-if [ ! -f "/usr/bin/docker" ]; then
-    yum -y install docker
-fi
-
-# NOTE(mandre) $docker_namespace_is_registry is not a bash variable but is
-# a place holder for text replacement done via heat
-if [ "$docker_namespace_is_registry" = "True" ]; then
-    /usr/bin/systemctl stop docker.service
-    # if namespace is used with local registry, trim all namespacing
-    trim_var=$docker_registry
-    registry_host="${trim_var%%/*}"
-    /bin/sed -i -r "s/^[# ]*INSECURE_REGISTRY *=.+$/INSECURE_REGISTRY='--insecure-registry $registry_host'/" /etc/sysconfig/docker
-fi
-
-# enable and start docker
-/usr/bin/systemctl enable docker.service
-/usr/bin/systemctl start docker.service
-
-# Disable libvirtd
+# Disable libvirtd since it conflicts with nova_libvirt container
 /usr/bin/systemctl disable libvirtd.service
 /usr/bin/systemctl stop libvirtd.service
