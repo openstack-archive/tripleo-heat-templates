7661d1f0085c2cc43dc78f4d783b45b1d17193f5
Ian Main <imain@redhat.com>
2017-03-20 17:04:52 -0400

    Change kolla_config from required to optional in pep8.
    We've decided to use volumes for configuration wherever possible.
    This means moving away from kolla_config blocks in the templates.
    Update pep8 to reflect this.
    
    Change-Id: If1ec40d0e5a515eed35e0cd04711079294f358c3

--- tools/yaml-validate.py
+++ tools/yaml-validate.py
@@ -23,11 +23,11 @@ envs_containing_endpoint_map = ['tls-endpoints-public-dns.yaml',
                                 'tls-endpoints-public-ip.yaml',
                                 'tls-everywhere-endpoints-dns.yaml']
 ENDPOINT_MAP_FILE = 'endpoint_map.yaml'
-REQUIRED_DOCKER_SECTIONS = ['service_name', 'docker_config', 'kolla_config',
-                            'puppet_config', 'config_settings', 'step_config']
+REQUIRED_DOCKER_SECTIONS = ['service_name', 'docker_config', 'puppet_config',
+                            'config_settings', 'step_config']
 OPTIONAL_DOCKER_SECTIONS = ['docker_puppet_tasks', 'upgrade_tasks',
                             'service_config_settings', 'host_prep_tasks',
-                            'metadata_settings']
+                            'metadata_settings', 'kolla_config']
 DOCKER_PUPPET_CONFIG_SECTIONS = ['config_volume', 'puppet_tags', 'step_config',
                                  'config_image']
 
