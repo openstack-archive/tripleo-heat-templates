a216934f408439e77bf8346dafe30c4752c70946
James Slagle <jslagle@redhat.com>
2017-04-03 12:50:45 -0400

    Purge initial firewall for deployed-server's
    We need to purge the initial firewall for deployed-server's, otherwise
    if you have a default REJECT rule, the pacemaker cluster will fail to
    initialize. This matches the behavior done when using images, see:
    Iddc21316a1a3d42a1a43cbb4b9c178adba8f8db3
    I0dee5ff045fbfe7b55d078583e16b107eec534aa
    
    Change-Id: Ia83d17b609e4f737074482a980689cc57c3ad911
    Closes-Bug: #1679234

--- deployed-server/deployed-server-bootstrap-centos.sh
+++ deployed-server/deployed-server-bootstrap-centos.sh
@@ -15,3 +15,6 @@ ln -s -f /usr/share/openstack-puppet/modules/* /etc/puppet/modules
 
 setenforce 0
 sed -i 's/^SELINUX=.*/SELINUX=permissive/' /etc/selinux/config
+
+echo '# empty ruleset created by deployed-server bootstrap' > /etc/sysconfig/iptables
+echo '# empty ruleset created by deployed-server bootstrap' > /etc/sysconfig/ip6tables
