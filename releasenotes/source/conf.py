b67f77e43bc12c90cab94b6d9b0032ec4b2c15ce
Emilien Macchi <emilien@redhat.com>
2017-04-27 12:17:46 -0400

    Prepare 6.1.0 (ocata)
    Change-Id: Idb0423f9cf76234b9f44cacf32dd34cd9ae4e655

--- releasenotes/source/conf.py
+++ releasenotes/source/conf.py
@@ -52,9 +52,9 @@ copyright = u'2017, TripleO Developers'
 # built documents.
 #
 # The full version, including alpha/beta/rc tags.
-release = '6.0.0'
+release = '6.1.0'
 # The short X.Y version.
-version = '6.0.0'
+version = '6.1.0'
 
 # The full version, including alpha/beta/rc tags.
 
