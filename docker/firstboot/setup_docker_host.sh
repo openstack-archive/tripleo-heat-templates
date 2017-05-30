5f109d08e74f2eb5fc84b605831a1495ddcb499e
Jiri Stransky <jistr@redhat.com>
2017-05-30 15:49:51 +0200

    Containerize virtlogd
    So far we've been using virtlogd running on the host, we should now be
    using virtlogd from a container.
    
    Co-Authored-By: Martin André <m.andre@redhat.com>
    Co-Authored-By: Jiri Stransky <jistr@redhat.com>
    Change-Id: I998c69ea1f7480ebb90afb44d6006953a84a1c04

--- docker/firstboot/setup_docker_host.sh
+++ docker/firstboot/setup_docker_host.sh
@@ -6,3 +6,6 @@ set -eux
 # Disable libvirtd since it conflicts with nova_libvirt container
 /usr/bin/systemctl disable libvirtd.service
 /usr/bin/systemctl stop libvirtd.service
+# Disable virtlogd since it conflicts with nova_virtlogd container
+/usr/bin/systemctl disable virtlogd.service
+/usr/bin/systemctl stop virtlogd.service
