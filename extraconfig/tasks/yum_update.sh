5a89ea21f2add98119a10464b020a98999d31c41
Alan Bishop <abishop@redhat.com>
2017-08-30 09:26:16 -0400

    Maintain ceph-osd package only on nodes hosting CephOSD service
    The ceph-osd package is only required on nodes hosting the CephOSD
    service, but the package's presence on other nodes may interfere with
    software updates. That's because some distros distribute Ceph software
    in different channels, and not all nodes have access to the ceph-osd
    channel.
    
    There are two parts to the fix, and the first is an enhancement to the
    yum update process. The process detects when the ceph-osd package is not
    required, and removes the package from the node.
    
    The second part takes ceph-osd out of the default list of packages
    needed by puppet-ceph. The ceph-osd package is listed only on the nodes
    hosting the CephOSD service.
    
    Closes-Bug: #1713292
    Change-Id: I7a581518ed25cf5f264abfaabfcf2041363a065b

--- extraconfig/tasks/yum_update.sh
+++ extraconfig/tasks/yum_update.sh
@@ -85,6 +85,9 @@ fi
 # special case https://bugs.launchpad.net/tripleo/+bug/1635205 +bug/1669714
 special_case_ovs_upgrade_if_needed
 
+# Resolve any RPM dependency issues before attempting the update
+yum_pre_update
+
 if [[ "$pacemaker_status" == "active" ]] ; then
     echo "Pacemaker running, stopping cluster node and doing full package update"
     node_count=$(pcs status xml | grep -o "<nodes_configured.*/>" | grep -o 'number="[0-9]*"' | grep -o "[0-9]*")
