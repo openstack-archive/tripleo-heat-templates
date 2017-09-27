openstack-archive/tripleo-heat-templates

commit 5a400f8011c482abd9c0b550f566bb452159a383

Sync undercloud stackrc w/ instack
This adds some extra parameters that we now set in instack-undercloud's
stackrc. Some of which are required for the latest keystone v3
support.

Change-Id: I7043d105ce8312656cb3f2ef23cfaded62c9cf83
Closes-bug: #1718991

extraconfig/post_deploy/undercloud_post.sh
@@ -3,22 +3,36 @@ set -eux
 
 ln -sf /etc/puppet/hiera.yaml /etc/hiera.yaml
 
-
 # WRITE OUT STACKRC
 if [ ! -e /root/stackrc ]; then
     touch /root/stackrc
     chmod 0600 /root/stackrc
 
-cat >> /root/stackrc <<-EOF_CAT
+    cat > /root/stackrc <<-EOF_CAT
+export OS_AUTH_TYPE=password
 export OS_PASSWORD=$admin_password
 export OS_AUTH_URL=$auth_url
 export OS_USERNAME=admin
 export OS_TENANT_NAME=admin
 export COMPUTE_API_VERSION=1.1
 export NOVA_VERSION=1.1
-export OS_BAREMETAL_API_VERSION=1.15
 export OS_NO_CACHE=True
 export OS_CLOUDNAME=undercloud
+# 1.34 is the latest API version in Ironic Pike supported by ironicclient
+export IRONIC_API_VERSION=1.34
+export OS_BAREMETAL_API_VERSION=\$IRONIC_API_VERSION
+export OS_IDENTITY_API_VERSION='3'
+export OS_PROJECT_DOMAIN_NAME='Default'
+export OS_USER_DOMAIN_NAME='Default'
+EOF_CAT
+
+    cat >> /root/stackrc <<-"EOF_CAT"
+# Add OS_CLOUDNAME to PS1
+if [ -z "${CLOUDPROMPT_ENABLED:-}" ]; then
+    export PS1=${PS1:-""}
+    export PS1=\${OS_CLOUDNAME:+"(\$OS_CLOUDNAME)"}\ $PS1
+    export CLOUDPROMPT_ENABLED=1
+fi
 EOF_CAT
 
     if [ -n "$ssl_certificate" ]; then

commit aae3cc1ef519eb4ce27841f22a6b1afa61fca6f2

Fix stackrc for containerized undercloud
Commit 5a400f8011c482abd9c0b550f566bb452159a383 broke the stackrc for
the containerized undercloud, making all the openstack commands fail
with:

  Cannot use v2 authentication with domain scope

This replaces the OS_TENANT_NAME variable with OS_PROJECT_NAME to and
switch to versionless auth_url to use keystone v3 instead.

Change-Id: I869adb75294b38c61e508870a69e1637bb410a94
Closes-Bug: #1719796

extraconfig/post_deploy/undercloud_post.sh
@@ -13,7 +13,7 @@ export OS_AUTH_TYPE=password
 export OS_PASSWORD=$admin_password
 export OS_AUTH_URL=$auth_url
 export OS_USERNAME=admin
-export OS_TENANT_NAME=admin
+export OS_PROJECT_NAME=admin
 export COMPUTE_API_VERSION=1.1
 export NOVA_VERSION=1.1
 export OS_NO_CACHE=True

extraconfig/post_deploy/undercloud_post.yaml
@@ -85,9 +85,9 @@ resources:
               scheme: http
               host: {get_param: [DeployedServerPortMap, 'control_virtual_ip', fixed_ips, 0, ip_address]}
               port: 5000
-              path: /v2.0
+              path: /
           - make_url:
               scheme: https
               host: {get_param: [DeployedServerPortMap, 'public_virtual_ip', fixed_ips, 0, ip_address]}
               port: 13000
-              path: /v2.0
+              path: /