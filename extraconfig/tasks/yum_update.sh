29f32c4c2c8d815f7ecc0c4c10eec6533bbca6d5
Emilien Macchi <emilien@redhat.com>
2017-10-17 11:13:03 -0700

    Do not rely on openstack-puppet-modules
    We are removing openstack-puppet-modules package since we now have
    individual packages for each Puppet module.
    We can still rely on puppet-tripleo module to deploy all dependencies.
    
    Change-Id: I5ec13e8db5ae5422da9cb8a59c69c9c6c8f34622
    Related-Bug: #1676998

--- extraconfig/tasks/yum_update.sh
+++ extraconfig/tasks/yum_update.sh
@@ -98,10 +98,10 @@ if [[ "$pacemaker_status" == "active" ]] ; then
         pcs cluster stop
     fi
 else
-    echo "Upgrading openstack-puppet-modules and its dependencies"
+    echo "Upgrading Puppet modules and dependencies"
     check_for_yum_lock
-    yum -q -y update openstack-puppet-modules
-    yum deplist openstack-puppet-modules | awk '/dependency/{print $2}' | xargs yum -q -y update
+    yum -q -y update puppet-tripleo
+    yum deplist puppet-tripleo | awk '/dependency/{print $2}' | xargs yum -q -y update
     echo "Upgrading other packages is handled by config management tooling"
     echo -n "true" > $heat_outputs_path.update_managed_packages
     exit 0
