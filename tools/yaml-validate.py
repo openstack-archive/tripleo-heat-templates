9a8c007daa5d02e20454a39ee453ea6e9f6b0292
Oliver Walsh <owalsh@redhat.com>
2020-11-12 20:38:26 +0000

    Move cell_v2 discovery off compute hosts
    In I12a02f636f31985bc1b71bff5b744d346286a95f cell_v2 discovery was
    originally moved from the nova-api container to the
    nova-compute|nova-ironic containers in order to run cell
    discovery during a scale up where the controllers are omitted
    (e.g to exclude the controllers from a maintenance window).
    
    This requires api database credentials on the compute node, which is
    forbidden, so it must move back to a nova-api host as a pre-requisite
    for removing these credentials in a follow-up patch.
    
    Scale-up while omitting the controllers will no longer work out of the
    box. Either a manual cell_v2 discovery can be run after scale up, or an
    additional node can be deployed using the NovaManager tripleo role.
    
    Related-bug: #1786961
    Related-bug: #1871482
    Change-Id: I47b95ad46e2d4e5b1f370a2f840826e87da2d703
    (cherry picked from commit 629485dde5d6b96d38688f362362fac45392ece9)
    (cherry picked from commit 03697234fd2e8c8d052c331485d304bcb77b1b8b)

--- tools/yaml-validate.py
+++ tools/yaml-validate.py
@@ -259,6 +259,10 @@ VALIDATE_DOCKER_OVERRIDE = {
   # Does not follow the filename pattern
   './deployment/multipathd/multipathd-container.yaml': True
 }
+VALIDATE_DOCKER_PUPPET_CONFIG_OVERRIDE = {
+    # inherits from nova-conductor
+  './deployment/nova/nova-manager-container-puppet.yaml': False,
+}
 DEPLOYMENT_RESOURCE_TYPES = [
     'OS::Heat::SoftwareDeploymentGroup',
     'OS::Heat::StructuredDeploymentGroup',
@@ -739,7 +743,8 @@ def validate_docker_service(filename, tpl):
                           % (section_name, filename))
                     return 1
 
-        if 'puppet_config' in role_data:
+        if 'puppet_config' in role_data and \
+                VALIDATE_DOCKER_PUPPET_CONFIG_OVERRIDE.get(filename, True):
             if validate_docker_service_mysql_usage(filename, tpl):
                 print('ERROR: could not validate use of mysql service for %s.'
                       % filename)
