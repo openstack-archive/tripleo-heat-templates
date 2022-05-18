b2a35f13d54ccbb3ad5842c66f9394b7ea265fd6
Takashi Kajinami <tkajinam@redhat.com>
2022-05-18 11:13:54 +0900

    Remove 'u' prefix
    In Python 3, u'...' and '...' are equivalent. Also, the prefix is no
    longer printed.
    
    Change-Id: I3de64b2526422322ad10231d9d682a326d4435ca

--- releasenotes/source/conf.py
+++ releasenotes/source/conf.py
@@ -44,8 +44,8 @@ source_suffix = '.rst'
 master_doc = 'index'
 
 # General information about the project.
-project = u'tripleo-heat-templates Release Notes'
-copyright = u'2017, TripleO Developers'
+project = 'tripleo-heat-templates Release Notes'
+copyright = '2017, TripleO Developers'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
@@ -199,8 +199,8 @@ latex_elements = {
 # (source start file, target name, title,
 #  author, documentclass [howto, manual, or own class]).
 latex_documents = [
-  ('index', 'tripleo-heat-templatesReleaseNotes.tex', u'tripleo-heat-templates Release Notes Documentation',
-   u'2016, TripleO Developers', 'manual'),
+  ('index', 'tripleo-heat-templatesReleaseNotes.tex', 'tripleo-heat-templates Release Notes Documentation',
+   '2016, TripleO Developers', 'manual'),
 ]
 
 # The name of an image file (relative to this directory) to place at the top of
@@ -229,8 +229,8 @@ latex_documents = [
 # One entry per manual page. List of tuples
 # (source start file, name, description, authors, manual section).
 man_pages = [
-    ('index', 'tripleo-heat-templatesreleasenotes', u'tripleo-heat-templates Release Notes Documentation',
-     [u'2016, TripleO Developers'], 1)
+    ('index', 'tripleo-heat-templatesreleasenotes', 'tripleo-heat-templates Release Notes Documentation',
+     ['2016, TripleO Developers'], 1)
 ]
 
 # If true, show URL addresses after external links.
@@ -243,8 +243,8 @@ man_pages = [
 # (source start file, target name, title, author,
 #  dir menu entry, description, category)
 texinfo_documents = [
-  ('index', 'tripleo-heat-templatesReleaseNotes', u'tripleo-heat-templates Release Notes Documentation',
-   u'2016, TripleO Developers', 'tripleo-heat-templatesReleaseNotes', 'One line description of project.',
+  ('index', 'tripleo-heat-templatesReleaseNotes', 'tripleo-heat-templates Release Notes Documentation',
+   '2016, TripleO Developers', 'tripleo-heat-templatesReleaseNotes', 'One line description of project.',
    'Miscellaneous'),
 ]
 
